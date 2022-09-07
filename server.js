0e830651615efb4d5e67c32abb1d81a5e02e94d3
wellington <wellington220100@gmail.com>
2022-09-07 19:04:01 -0300

    version 1.0

--- server.js
+++ server.js
@@ -1,17 +1,22 @@
 const express = require('express')
+const { resolve } = require('path')
 const path = require('path');
-
 const app = express()
 
+//Estáticos -> Roteamento dos Componentes
 app.use('/',
-    express.static(
-        path.join(
-            __dirname,
-            'dist'
-        )
+  express.static(
+    resolve(
+      __dirname,
+      './dist'
     )
-)
+  )
+) 
 
-app.listen(process.env.PORT || 3000, (err) =>{
-    err ? console.log("err", err) : console.log("Tudo certo")
+ app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 })
+
+app.listen(process.env.PORT || 3000, (err) => {
+  err ? console.log("err", err) : console.log("Tudo Funcionando!")
+});
