98728d9254210956624a2f28e2b2ec0b353a99d7
wellington <wellington220100@gmail.com>
2022-09-07 18:28:29 -0300

    version 1.0

--- server.js
+++ server.js
@@ -13,7 +13,8 @@ app.use('/',
     )
 )
 
-app
-    .listen(process.env.PORT || 3000, function(){
-        console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-    })
+app.listen(process.env.PORT || 3000, (err) => {
+    if(err) {return console.log(err)}
+
+    console.log('Tudo certo')
+})
